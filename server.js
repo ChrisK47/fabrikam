bcd262f9e4943e8c54fee8cc645a7e4cd32f5331
ChrisK47 <chriskalyan12@gmail.com>
2022-02-21 18:06:02 +0530

    Second Upload

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express =require("express");  //lib-----Node Module
-var app=express();  // will create app object from express package
+var express = require("express");  //lib-----Node Module
+var app = express();  // will create app object from express package
 
-var onAboutUs=function(req, res){
+var onAboutUs = function (req, res) {
     res.send("Chief Mentor :Ravi Tambade");
 };
-var onDefault=function(req, res){
-    res.send("<h1>fabrikam Pvt. Ltd</h1>"+
-              "<hr/>"+
-              "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
-             "</ol>");
+var onDefault = function (req, res) {
+    res.send("<h1>Fabrikam Pvt. Ltd</h1>" +
+        "<hr/>" +
+        "<ol>" +
+        " <li>Human Resources</li>" +
+        " <li>Accounting and Project Management</li>" +
+        " <li>Product </li>" +
+        " <li>Services</li>" +
+        "</ol>");
 };
-app.get("/",onDefault);  // Request handler functions are registered
-app.get("/aboutus",onAboutUs);  // Request handler functions are registered
-var server=app.listen(8081);
+app.get("/", onDefault);  // Request handler functions are registered
+app.get("/aboutus", onAboutUs);  // Request handler functions are registered
+var server = app.listen(8081);
 console.log("Server is running on port 8081");
